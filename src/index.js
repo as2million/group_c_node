eed48ef6fb3d0f28f1258195709c102ad798cdc4
Claudia <sydeng102@gmail.com>
2020-11-04 21:00:01 +0800

    20201104 09:00p.m db connect test

--- src/index.js
+++ src/index.js
@@ -1,23 +1,35 @@
 const express = require('express');
+const multer = require('multer');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 const app = express();
+const axios = require('axios');
+const jwt = require('jsonwebtoken');
+const session = require('express-session');
+const MysqlStore = require('express-mysql-session')(session);
+const moment = require('moment-timezone');
+const cors = require('cors');
+const db = require(__dirname + '/db_connect');
+const sessionStore = new MysqlStore({}, db);
+const upload = multer({ dest: __dirname + '/../tmp_uploads' });
 
-app.use( express.urlencoded({extended: false}));
-app.use( express.json() );
-
-
-
-
-// app.use( express.static,require(__dirname + '/public'));
-// app.use( '/login-api',require(__dirname + '/src/Cha/cha'));
-
-
-
-
-//使用靜態內容的資料夾
-// app.use( express.static(__dirname + '/../public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 
-// Server 偵聽
-app.listen(process.env.PORT || 5000, ()=>{
+//連線資料庫
+app.get('/try-db', (req, res) => {
+    db.query('SELECT * FROM taiwan_farms LIMIT 5')
+        .then(([results]) => {
+            res.json(results);
+        })
+});
+
+
+// app.use(express.static, require(__dirname + '/../public'));
+// app.use('/login-api', require(__dirname + '/Cha/cha'));
+
+// Server
+app.listen(5000, () => {
     console.log('伺服器已啟動...');
 })
