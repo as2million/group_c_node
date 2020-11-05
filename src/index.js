c777713d422abbcae932577eb480e570a09fe38f
tseng-yc <janice8819@gmail.com>
2020-11-05 17:28:09 +0800

    20201105 05:27p.m get member_list data

--- src/index.js
+++ src/index.js
@@ -17,19 +17,40 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
+const corsOptions = {
+  // 跨網域 操作cookie 要設定true
+  credentials: true,
+  //每次進來都會拿到一個cb
+  origin: function (origin, cb) {
+    console.log(`origin: ${origin}`)
+    //前面是錯誤 後面是允許
+    cb(null, true)
+  },
+}
+app.use(cors(corsOptions))
+
 //連線資料庫
-app.get('/try-db', (req, res) => {
-    db.query('SELECT * FROM taiwan_farms LIMIT 5')
-        .then(([results]) => {
-            res.json(results);
-        })
-});
+// app.get('/member_list', (req, res) => {
+//     db.query('SELECT * FROM member_list LIMIT 5')
+//         .then(([results]) => {
+//             res.json(results);
+//         })
+// });
+
+// app.get('/product', (req, res) => {
+//     db.query('SELECT * FROM product LIMIT 5')
+//         .then(([results]) => {
+//             res.json(results);
+//         })
+// });
 
 
 app.use(express.static(__dirname + '/../public'));
 
 //範例
 app.use('/example', require(__dirname + '/Name/example'));
+
+
 
 
 //引入的檔案裡面一定要有東西，不然會報錯，所以先註解掉
@@ -44,7 +65,7 @@ app.use('/example', require(__dirname + '/Name/example'));
 // app.use('/member', require(__dirname + '/Iris'));
 
 //Janice
-// app.use('/index', require(__dirname + '/Janice'));
+app.use('/index', require(__dirname + '/Janice/janice'));
 
 //Jess
 // app.use('/bento', require(__dirname + '/Jess'));
