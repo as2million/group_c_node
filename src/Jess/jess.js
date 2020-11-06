43e8dcb9dac22c43569d103cdff7deb6ef8ba641
Claudia <sydeng102@gmail.com>
2020-11-06 10:31:16 +0800

    20201106 10:30a.m fix totalrows sql

--- src/Jess/jess.js
+++ src/Jess/jess.js
@@ -12,51 +12,98 @@ const db = require(__dirname + '/../db_connect');
 
 //連線資料庫
 router.get('/bento', (req, res) => {
-    db.query('SELECT * FROM `product` WHERE 1')
-        .then(([results]) => {
-            res.json(results);
-        })
+  db.query('SELECT * FROM `product` WHERE 1')
+    .then(([results]) => {
+      res.json(results);
+    })
 });
 
 //連線資料庫
 router.get('/message', (req, res) => {
-    db.query('SELECT * FROM `message` WHERE 1')
-        .then(([results]) => {
-            res.json(results);
-        })
+  db.query('SELECT * FROM `message` WHERE 1')
+    .then(([results]) => {
+      res.json(results);
+    })
 });
 
 // 只取sid=1
 router.get('/productSid', (req, res) => {
-    db.query('SELECT * FROM `message` WHERE product_sid=1')
-        .then(([results]) => {
-            res.json(results);
-        })
+  db.query('SELECT * FROM `message` WHERE product_sid=1')
+    .then(([results]) => {
+      res.json(results);
+    })
 });
 
 //取product_sid=1 + 會員名字
 router.get('/member', (req, res) => {
-    db.query('SELECT member_list.name,message.content,message.product_sid FROM `member_list` INNER JOIN message on  member_list.member_sid=message.member_sid WHERE product_sid=1')
-        .then(([results]) => {
-            res.json(results);
-        })
-    
+  db.query('SELECT member_list.name,message.content,message.product_sid FROM `member_list` INNER JOIN message on  member_list.member_sid=message.member_sid WHERE product_sid=1')
+    .then(([results]) => {
+      res.json(results);
+    })
+
 });
 
-//處理分頁
-router.get("/productSid2", async (req, res) => {
+// //處理分頁
+// router.get("/productSid2", async (req, res) => {
+//   const output = {
+//     page: 0,
+//     perPage: 5,
+//     totalRows: 0,
+//     totalPages: 0,
+//     rows: [],
+//   }
+//   //desctructor  總筆數
+//   //這裡只取第一個值  還是arrary
+//   const [[{ totalRows }]] = await db.query(
+//     "SELECT * FROM `message` WHERE product_sid=1"
+//   )
+//   if (totalRows > 0) {
+//     //沒有輸入值就是1  轉換成整數
+//     let page = parseInt(req.query.page) || 1
+//     output.totalRows = totalRows
+//     //會回傳大於等於所給數字的最小整數
+//     output.totalPages = Math.ceil(totalRows / output.perPage)
+
+//     //拿到的值做判斷
+//     if (page < 1) {
+//       output.page = 1
+//     } else if (page > output.totalPages) {
+//       output.page = totalPages
+//     } else {
+//       output.page = page
+//     }
+//     // //分頁資料 //起始位置
+//     // let sql = `SELECT * FROM message ORDER BY sid DESC LIMIT ${
+//     //   (output.page - 1) * output.perPage
+//     // }, ${output.perPage}`
+
+//     // const [r2] = await db.query(sql)
+
+//     // output.rows = r2
+//   }
+//   res.json(output)
+// })
+
+async function getListData(req) {
+
+  // const sql = "SELECT COUNT(1) totalRows FROM `message` WHERE product_sid=1"
+  // const [[{ totalRows }]] = await db.query(sql)
+  // return totalRows;
+
+
   const output = {
     page: 0,
     perPage: 5,
     totalRows: 0,
     totalPages: 0,
-    rows: [],
+    rows: []
   }
   //desctructor  總筆數
   //這裡只取第一個值  還是arrary
   const [[{ totalRows }]] = await db.query(
-    "SELECT * FROM `message` WHERE product_sid=1"
-  )
+    "SELECT COUNT(1) totalRows FROM `message` WHERE product_sid=1"
+  );
+
   if (totalRows > 0) {
     //沒有輸入值就是1  轉換成整數
     let page = parseInt(req.query.page) || 1
@@ -73,15 +120,19 @@ router.get("/productSid2", async (req, res) => {
       output.page = page
     }
     // //分頁資料 //起始位置
-    // let sql = `SELECT * FROM message ORDER BY sid DESC LIMIT ${
-    //   (output.page - 1) * output.perPage
-    // }, ${output.perPage}`
+    let sql = `SELECT * FROM message ORDER BY sid DESC LIMIT ${(output.page - 1) * output.perPage
+      }, ${output.perPage}`
 
-    // const [r2] = await db.query(sql)
-
-    // output.rows = r2
+    const [r2] = await db.query(sql);
+    output.rows = r2
   }
-  res.json(output)
+  return output;
+}
+
+router.get("/productSid2", async (req, res) => {
+  res.json(await getListData(req))
 })
+
+
 
 module.exports = router;
