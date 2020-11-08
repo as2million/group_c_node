87700590765b4691bda3bfb17c55c99c49eb1366
RuFrontEnd <50475511+RuFrontEnd@users.noreply.github.com>
2020-11-09 02:20:53 +0800

    20201109 02:20a.m. 建立客製化API

--- src/index.js
+++ src/index.js
@@ -1,36 +1,42 @@
-const express = require('express');
-const multer = require('multer');
-const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
+const express = require("express");
+const multer = require("multer");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 const app = express();
-const axios = require('axios');
-const jwt = require('jsonwebtoken');
-const session = require('express-session');
-const MysqlStore = require('express-mysql-session')(session);
-const moment = require('moment-timezone');
-const cors = require('cors');
-const db = require(__dirname + '/db_connect');
+const axios = require("axios");
+const jwt = require("jsonwebtoken");
+const session = require("express-session");
+const MysqlStore = require("express-mysql-session")(session);
+const moment = require("moment-timezone");
+const cors = require("cors");
+const db = require(__dirname + "/db_connect");
 const sessionStore = new MysqlStore({}, db);
-const upload = multer({ dest: __dirname + '/../tmp_uploads' });
+const upload = multer({ dest: __dirname + "/../tmp_uploads" });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// 允許跨網域
+const corsOptions = {
+  credentials: true,
+  origin: function (origin, cb) {
+    console.log(`origin: ${origin}`);
+    cb(null, true);
+  },
+};
+app.use(cors(corsOptions));
 
 //連線資料庫
-app.get('/try-db', (req, res) => {
-    db.query('SELECT * FROM taiwan_farms LIMIT 5')
-        .then(([results]) => {
-            res.json(results);
-        })
-});
+// app.get("/try-db", (req, res) => {
+//   db.query("SELECT * FROM taiwan_farms LIMIT 5").then(([results]) => {
+//     res.json(results);
+//   });
+// });
 
-
-app.use(express.static(__dirname + '/../public'));
+app.use(express.static(__dirname + "/../public"));
 
 //範例
-app.use('/example', require(__dirname + '/Name/example'));
-
+app.use("/example", require(__dirname + "/Name/example"));
 
 //引入的檔案裡面一定要有東西，不然會報錯，所以先註解掉
 
@@ -50,10 +56,9 @@ app.use('/example', require(__dirname + '/Name/example'));
 // app.use('/bento', require(__dirname + '/Jess'));
 
 //Ru
-// app.use('/product', require(__dirname + '/Ru'));
-
+app.use("/product", require(__dirname + "/Ru/ru"));
 
 // Server
 app.listen(5000, () => {
-    console.log('伺服器已啟動');
-})
+  console.log("伺服器已啟動");
+});
